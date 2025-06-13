3b7b977d38b44357a10e62a034e5500f5124507e
Róbert Göndöcs <gondocs.robert@gmail.com>
2025-06-13 02:31:40 +0200

    add svg

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,113 +6,113 @@ const about = {
   frontend: [
     {
       name: "React.js",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      logo: "react-original.svg",
       link: "https://react.dev/",
     },
     {
       name: "JavaScript (ES6+)",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      logo: "javascript-original.svg",
       link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
       name: "TypeScript",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+      logo: "typescript-original.svg",
       link: "https://www.typescriptlang.org/",
     },
     {
       name: "HTML5 & CSS3",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      logo: "html5-original.svg",
       link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5",
     },
     {
       name: "Material UI",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+      logo: "materialui-original.svg",
       link: "https://mui.com/",
     },
     {
       name: "Tailwind CSS",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/768px-Tailwind_CSS_Logo.svg.png?20230715030042",
+      logo: "Tailwind_CSS_Logo.svg",
       link: "https://tailwindcss.com/",
     },
     {
       name: "Bootstrap",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+      logo: "bootstrap-original.svg",
       link: "https://getbootstrap.com/",
     },
     {
       name: "UI/UX Design, Figma",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+      logo: "figma-original.svg",
       link: "https://www.figma.com/",
     },
     {
       name: "WordPress",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg",
+      logo: "wordpress-original.svg",
       link: "https://wordpress.org/",
     },
   ],
   backend: [
     {
       name: "PHP",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+      logo: "php-original.svg",
       link: "https://www.php.net/",
     },
     {
       name: "MySQL",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+      logo: "mysql-original.svg",
       link: "https://www.mysql.com/",
     },
     {
       name: "MSSQL",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain.svg",
+      logo: "microsoftsqlserver-plain.svg",
       link: "https://www.microsoft.com/en-us/sql-server",
     },
     {
       name: "C#",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+      logo: "csharp-original.svg",
       link: "https://learn.microsoft.com/en-us/dotnet/csharp/",
     },
     {
       name: "Unity",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg",
+      logo: "unity-original.svg",
       link: "https://unity.com/",
     },
     {
       name: "Plesk",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Logo_Plesk.svg/768px-Logo_Plesk.svg.png?20200804133750",
+      logo: "plesk-logo.svg",
       link: "https://www.plesk.com/",
     },
   ],
   tools: [
     {
       name: "Git",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      logo: "git-original.svg",
       link: "https://git-scm.com/",
     },
     {
       name: "GitHub",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+      logo: "github-original.svg",
       link: "https://github.com/",
     },
     {
       name: "GitLab",
-      logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg",
+      logo: "gitlab-original.svg",
       link: "https://gitlab.com/",
     },
   ],
   data: [
     {
       name: "Google Analytics",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_Google_Analytics.svg",
+      logo: "google_analytics-icon.svg",
       link: "https://analytics.google.com/",
     },
     {
       name: "GDPR Cookie",
-      logo: "https://cdn-icons-png.flaticon.com/512/1047/1047711.png",
+      logo: "cookie-icon.svg",
       link: "https://gdpr.eu/cookies/",
     },
     {
       name: "Looker Studio",
-      logo: "https://lookerstudio.google.com/favicon.ico",
+      logo: "looker-icon.svg",
       link: "https://lookerstudio.google.com/",
     },
   ],
@@ -128,7 +128,7 @@ export default function About() {
         </h1>
         <div className="bg-black text-white rounded-xl my-4 p-6 shadow-lg">
           <p className="text-white mb-4 animate-slide-in-right">{about.bio}</p>
-          <h2 className="text-2xl font-semibold mt-6 mb-2">Frontend</h2>
+          <h2 className="text-2xl font-semibold mt-20 mb-2">Frontend</h2>
           <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 mb-6">
             {about.frontend.map((skill) => (
               <li
@@ -151,7 +151,7 @@ export default function About() {
               </li>
             ))}
           </ul>
-          <h2 className="text-2xl font-semibold mt-12 mb-2">Backend</h2>
+          <h2 className="text-2xl font-semibold mt-20 mb-2">Backend</h2>
           <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 mb-6">
             {about.backend.map((skill) => (
               <li
@@ -174,8 +174,8 @@ export default function About() {
               </li>
             ))}
           </ul>
-          <h2 className="text-2xl font-semibold mt-12 mb-2">Tools</h2>
-          <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+          <h2 className="text-2xl font-semibold mt-20 mb-2">Tools</h2>
+          <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 mb-6">
             {about.tools.map((tool) => (
               <li
                 key={tool.name}
@@ -197,8 +197,8 @@ export default function About() {
               </li>
             ))}
           </ul>
-          <h2 className="text-2xl font-semibold mt-12 mb-2">Data & Analytics</h2>
-          <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+          <h2 className="text-2xl font-semibold mt-20 mb-2">Data & Analytics</h2>
+          <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 mb-6">
             {about.data.map((item) => (
               <li
                 key={item.name}
